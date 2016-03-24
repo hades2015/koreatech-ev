ca84a00e07df5bbe8bdaee4eee006cb1ad4894f8
Mr_Chae <coalfm123@gmail.com>
2016-03-25 01:36:50 +0900

    loginPost는 수정이 필요할듯. 시큐리티가 Login의 Post메시지를 가져가서 저 부분은 실행이 되지않음. 세션부분은 시큐리티에서 가져오기 때문에 실행하지 제거. Logout부분도 시큐리티에서 처리하므로 제거. 사용자 가입 부분은 회원가입후 회원가입아이디의 접근권한 설정부분이 추가되었기 때문에 접근권한을 DB에 입력하는 부분도 추가.

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,63 +14,58 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import model.Student;
+import security.SecurityStudentService;
 import service.StudentService;
 import utils.StatusCode;
 
 @Controller
 public class StudentController {
+	
 	@Autowired
 	private StudentService studentService;
 	
 	@Autowired
-	ObjectMapper jacksonObjectMapper;
+	private SecurityStudentService secuService;
 	
+	@Autowired
+	ObjectMapper jacksonObjectMapper;
+
 	@RequestMapping("/")
 	public String root() {
 		return "redirect:login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginView() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public @ResponseBody JsonNode loginPost(@RequestParam String userId, 
-					@RequestParam String password, HttpSession sessions) {
-		
+	public @ResponseBody JsonNode loginPost(@RequestParam String userId, @RequestParam String password) {
+
 		ObjectNode resultMap = jacksonObjectMapper.createObjectNode();
 		Student student = studentService.readStudentByUserId(userId);
-		
-		if(password.equals(student.getPassword())) {
-			sessions.setAttribute("userId", student.getUserId());
-			sessions.setAttribute("userName", student.getUserName());
-			sessions.setAttribute("nickname", student.getNickname());
-			sessions.setAttribute("department", student.getDepartment());
-			sessions.setAttribute("grade", student.getGrade());
-			sessions.setAttribute("studentNumber", student.getStudentNumber());
-			sessions.setAttribute("userEmail", student.getUserEmail());
-			
+
+		System.out.println("login()");
+
+		if (password.equals(student.getPassword())) {
 			resultMap.put("statusCode", StatusCode.OK);
 		} else {
 			resultMap.put("statusCode", StatusCode.ERROR);
-		}	
+		}
 		return resultMap;
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signUpView() {
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public @ResponseBody JsonNode signUpPost(@ModelAttribute Student student) {
-		return studentService.createStudent(student);
-	}
-	@RequestMapping(value = "/logout")
-	public String logout(HttpSession session){
-		
-		session.invalidate();
-		return "redirect:login";
+		System.out.println(student.getUserId());
+		JsonNode signupResult = studentService.createStudent(student);
+		secuService.craeteUserRole(student.getUserId());
+		return signupResult;
 	}
 }
