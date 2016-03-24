5f25047b2d267cb268e363be21b07b0dc7336d9a
Mr_Chae <coalfm123@gmail.com>
2016-03-24 19:48:43 +0900

    name -> userName

--- src/main/java/sqls/StudentSqls.java
+++ src/main/java/sqls/StudentSqls.java
@@ -1,13 +1,13 @@
 package sqls;
 
 public interface StudentSqls {
-	String CREATE_STUDENT = "INSERT INTO student(user_id, password, name, nickname, department, grade, " +
+	String CREATE_STUDENT = "INSERT INTO student(user_id, password, userName, nickname, department, grade, " +
 			"student_number, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 	
-	String READ_STUDENT_BY_USER_ID = "SELECT user_id, password, name, nickname, department, grade, " +
+	String READ_STUDENT_BY_USER_ID = "SELECT user_id, password, userName, nickname, department, grade, " +
 			"student_number, email FROM student WHERE user_id = ?";
 	
-	String UPDATE_STUDENT_BY_USER_ID = "UPDATE student SET password = ?, name = ?, nickname = ?, " +
+	String UPDATE_STUDENT_BY_USER_ID = "UPDATE student SET password = ?, userName = ?, nickname = ?, " +
 			"department = ?, grade = ?, student_number = ?, email = ? WHERE user_id = ?";
 	
 	String DELETE_STUDENT_BY_USER_ID = "DELETE FROM student WHERE userId = ?";
