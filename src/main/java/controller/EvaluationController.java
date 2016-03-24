e53476f7630d6a72dfdc92da245426905ac377af
Mr_Chae <coalfm123@gmail.com>
2016-03-25 01:26:19 +0900

    - 로그인 후 시큐리티 컨텍스트에 존재하는 로그인 유저의 정보를 세션으로 유지.

--- src/main/java/controller/EvaluationController.java
+++ src/main/java/controller/EvaluationController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,21 +10,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/evaluation")
 @Controller
 public class EvaluationController {
-	
+
 	@RequestMapping("")
-	public String evaluationMain() {
+	public String evaluationMain(HttpSession session) {
+
+		Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		session.setAttribute("userInfo", user);
+
 		return "evaluation";
 	}
-	
+
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public String createEvaluationView() {
-		
+
 		return "new_evaluation";
 	}
-	
+
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createEvaluation() {
-		
+
 		return "redirect:/evaluation/detail";
 	}
 }
