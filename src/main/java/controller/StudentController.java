a326800a4ac514f62caebb0c46043a855626b6ec
Mr_Chae <coalfm123@gmail.com>
2016-03-24 19:48:29 +0900

    1. 스프링 시큐리티 적용. 2. 회원가입시 사용자권한을 추가해주는 부분 추가.

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import model.Student;
+import service.SecurityStudentService;
 import service.StudentService;
 
 @Controller
 public class StudentController {
 	@Autowired
 	private StudentService studentService;
+	
+	@Autowired
+	private SecurityStudentService securityService;
 	
 	@RequestMapping("/")
 	public String root() {
@@ -28,30 +31,6 @@ public class StudentController {
 		return "login";
 	}
 	
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String loginPost(@RequestParam String userId, @RequestParam String password, HttpSession sessions) {
-		
-		Student student = studentService.readStudentByUserId(userId);
-		
-		if(password.equals(student.getPassword())) {
-			
-			sessions.setAttribute("userId", student.getUserId());
-			sessions.setAttribute("userName", student.getUserName());
-			sessions.setAttribute("nickname", student.getNickname());
-			sessions.setAttribute("department", student.getDepartment());
-			sessions.setAttribute("grade", student.getGrade());
-			sessions.setAttribute("studentNumber", student.getStudentNumber());
-			sessions.setAttribute("userEmail", student.getUserEmail());
-			
-			return "redirect:evaluation";
-		
-		} else {
-		
-			return "redirect:login_fail";
-		
-		}	
-	}
-	
 	@RequestMapping(value = "signup", method = RequestMethod.GET)
 	public String signUpView() {
 		return "signup";
@@ -61,12 +40,9 @@ public class StudentController {
 	public String signUpPost(@ModelAttribute Student student) {
 		
 		studentService.createStudent(student);
-		return "redirect:login";
-	}
-	@RequestMapping(value = "/logout")
-	public String logout(HttpSession session){
+		securityService.craeteUserRole(student.getUserId());
 		
-		session.invalidate();
 		return "redirect:login";
 	}
+
 }
